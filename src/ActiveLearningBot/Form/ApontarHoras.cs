27d81196535ff2c6cb3b6f9c28e7afaba5213cf0
William Pinto <william.pinto@lambda3.com.br>
2018-04-13 09:57:53 -0300

    Added validation to form

--- src/ActiveLearningBot/Form/ApontarHoras.cs
+++ src/ActiveLearningBot/Form/ApontarHoras.cs
@@ -26,14 +26,29 @@ namespace ActiveLearningBot.Form
         [Describe("Horas")]
         public int Hours { get; set; }
 
-
-
         public static IForm<ApontarHoras> BuildForm()
         {
             var form = new FormBuilder<ApontarHoras>();
             form.Configuration.DefaultPrompt.ChoiceStyle = ChoiceStyleOptions.Buttons;
             form.Configuration.Yes = new string[] { "sim", "yes", "s", "y", "yep", "é", "ye", "eh", "claro", "yeap", "quero", "quero sim", "isso", "é isso aí", "uhum"};
             form.Configuration.No = new string[] { "não", "nao", "no", "not", "n", "nem", "nop", "not", "errado", "mudei de ideia", "nem" };
+
+            form.Field(nameof(ProjectName))
+                .Field(nameof(Day))
+                .Field(nameof(TaskExecuted))
+                .Field(nameof(Hours), validate: async (state, value) =>
+                {
+                    var choosedHour = Convert.ToInt32(value);
+                    var result = new ValidateResult { IsValid = true, Value = choosedHour };
+
+                    if (choosedHour > 8 || choosedHour < 0)
+                    {
+                        result.Feedback = "A hora deve ser um número entre 0 e 8";
+                        result.IsValid = false;
+                    }
+                    return result;
+                });
+
             return form.Build();
         }
     }
